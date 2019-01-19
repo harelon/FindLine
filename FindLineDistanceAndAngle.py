73e40ee90de9948377079f66a7701b25f706ebc8
Harel <harel.games@gmail.com>
2019-01-19 22:54:54 +0200

    changed picture names and adjusted finding line

--- FindLineDistanceAndAngle.py
+++ FindLineDistanceAndAngle.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import sys
+import argparse
 
 counter = 0
 def best_contour(image):
@@ -18,10 +19,10 @@ def best_contour(image):
         rect = cv2.minAreaRect(contour)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
-        if cv2.contourArea(box) == 0:
+        if cv2.contourArea(box) <= 200 or cv2.contourArea(contour)==0 :
             continue
-        if cv2.contourArea(box) - cv2.contourArea(contour) < minDiffer and cv2.contourArea(box) > 100 and len(contour)<20:
-            minDiffer =  cv2.contourArea(box) - cv2.contourArea(contour)
+        if cv2.contourArea(box) / cv2.contourArea(contour) < minDiffer:
+            minDiffer =  cv2.contourArea(box) / cv2.contourArea(contour)
             cont = contour
             cont_area = cv2.contourArea(contour)
     output = cv2.drawContours(img_color, cont, -1, (0, 0, 255), 4) 
@@ -29,7 +30,14 @@ def best_contour(image):
     counter=counter+1
     return (cont, cont_area, is_single)
 
-image = cv2.imread(".\output 13.5cm\d200ccd-10lcr0.jpg")
+parser = argparse.ArgumentParser(
+        description='An image white line will be detected in')
+parser.add_argument(
+        'image',
+        help='the path of the image'
+    )
+args = parser.parse_args()
+image = cv2.imread(args.image)
 cv2.imshow("base image", image)
 
 image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
@@ -37,26 +45,18 @@ cv2.imshow("grayscale image", image)
 
 image_half_y = int(image.shape[0] / 2)
 roi = image[image_half_y:image.shape[0], 0:image.shape[1]]
-ret, roi = cv2.threshold(roi, 210, 255, cv2.THRESH_BINARY)
+ret, roi = cv2.threshold(roi, 200, 255, cv2.THRESH_BINARY)
 cv2.imshow("lower half after threshold", roi)
 
 kernel = np.ones((5, 5), np.uint8)
-roi_opened = cv2.morphologyEx(roi, cv2.MORPH_OPEN, kernel)
-cv2.imshow("lower half after threshold and open", roi_opened)
+roi_morphed = cv2.erode(roi, kernel, iterations = 1)
+roi_morphed = cv2.dilate(roi_morphed, kernel, iterations = 1)
+cv2.imshow("lower half after threshold and 3 erodes 1 dilate", roi_morphed)
 
-roi_closed = cv2.morphologyEx(roi, cv2.MORPH_CLOSE, kernel)
-cv2.imshow("lower half after threshold and close", roi_closed)
-
-opened_result = best_contour(roi_opened)
-closed_result = best_contour(roi_closed)
-output = None
+roi_morphed_result = best_contour(roi_morphed)
+img_color = cv2.cvtColor(roi_morphed,cv2.COLOR_GRAY2BGR)
 try:
-    if(opened_result[1]<closed_result[1] or closed_result[2]) and not opened_result[2]:        
-        img_color = cv2.cvtColor(roi_closed,cv2.COLOR_GRAY2BGR)
-        output = cv2.drawContours(img_color,[closed_result[0]],-1,(0,0,255),4)
-    else:
-        img_color = cv2.cvtColor(roi_opened,cv2.COLOR_GRAY2BGR)
-        output = cv2.drawContours(img_color,[opened_result[0]],-1,(0,0,255),4)    
+    output = cv2.drawContours(img_color,[roi_morphed_result[0]],-1,(0,0,255),4)
 except:
     output=roi
     pass
